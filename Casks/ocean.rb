1e6e1734c2c004330753f55fbbcaf627c97c7653
Ankit Yadav <4nkitd@gmail.com>
2026-08-16 16:13:49 +0530

    cask: bump ocean to 1.1.0

--- Casks/ocean.rb
+++ Casks/ocean.rb
@@ -1,6 +1,6 @@
 cask "ocean" do
-  version "1.0.0"
-  sha256 "810756bdbe593b78ea35fe1cd7c2e9d6b711db69e695ad83dfd28a2e512b2aaf"
+  version "1.1.0"
+  sha256 "5eaf82a2eb3a74059a74b5b3c750dab7f235989b491d0c8e0417ac4d8ee3ce55"
 
   url "https://github.com/4nkitd/ocean/releases/download/v#{version}/Ocean-#{version}.dmg"
   name "Ocean"
