64580f8b3f0403d5b65844dfccf339380e28b63d
Ankit Yadav <4nkitd@users.noreply.github.com>
2026-08-21 09:28:25 +0530

    Update MP-1 cask to 0.1.1

--- Casks/mp-1.rb
+++ Casks/mp-1.rb
@@ -1,6 +1,6 @@
 cask "mp-1" do
-  version "0.1.0"
-  sha256 "9280256f8ddf83e0773e108b09a5c41b11abf07296eff2fac8b2bfa8544337ea"
+  version "0.1.1"
+  sha256 "cd94c193c24ec528dbb931ebc202f1e92a78deb124171f75d684fe0e7572e90d"
 
   url "https://github.com/4nkitd/mp-1/releases/download/v#{version}/MP-1-#{version}.dmg"
   name "MP-1"
